e2e5073e9853eb681d6f43ab9a9e5bbd2c1b6e0c
danfio7 <daniele.fiorucci@iisgubbio.edu.it>
2025-10-22 11:57:13 +0200

    prima forma di accesso

--- src/main/java/it/edu/iisgubbio/server/Server.java
+++ src/main/java/it/edu/iisgubbio/server/Server.java
@@ -11,19 +11,38 @@ public class Server {
 	@OnMessage
 	public void visualizzaRichiesta(Session session, String msg) {
 		System.out.println(msg);
-		try {
+		boolean accesso = false;
+		//try {
 			if (session.isOpen()) {
-				session.getBasicRemote().sendText(msg);
-				System.out.println("so felice " + msg);
+				//session.getBasicRemote().sendText(msg);
+				String x[] = msg.split("\\|");
+				String username = x[1];
+				String pwd = x[2];
+				System.out.println("nome= "+username);
+				System.out.println("pwd= "+pwd);
+				if(username.toUpperCase().equals(username)) {
+					if(pwd.charAt(1)=='l') {
+						System.out.println("ENTRATO");
+						accesso=true;
+					}else {
+						System.out.println("NON ENTRATO");
+					}
+				}else {
+					System.out.println("nome errato");
+					
+				}
+				if(accesso) {
+					
+				}
 			}
-		} catch (IOException e) {
-			try {
-
-				session.close();
-			} catch (IOException el) {
-				// niente
-			}
-		}
+//		} catch (IOException e) {
+//			try {
+//
+//				session.close();
+//			} catch (IOException el) {
+//				// niente
+//			}
+//		}
 	
 	
 //    	response.getWriter().write("ciao");q
