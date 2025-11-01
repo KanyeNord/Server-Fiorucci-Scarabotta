d3e77125526ed3f2998a38f8d73e64de5cbf6e61
KanyeNord <alessio.scarabotta@iisgubbio.edu.it>
2025-11-01 17:48:50 +0100

    completato il broadcast, aggiornato il file README, quasi completati i controlli per l'accesso, iniziati i controlli per il messaggio, aggiunti commenti per spiegare il codice

--- README.md
+++ README.md
@@ -7,3 +7,26 @@ Il nostro server funziona da host per una chat di gruppo, dove dei client posson
 ## Progettazione
 
  Il server è stato scritto con il linguaggio Java, nell'IDE Eclipse. 
+
+## Installazione
+
+### Scaricare il necessario
+
+-scaricare Tomcat 10.1.48 
+(https://tomcat.apache.org/download-10.cgi)
+
+-Scaricare Eclipse IDE for Enterprise Java and Web Developers, la nostra versione è la 2025-09
+(https://www.eclipse.org/downloads/packages/release/2025-09/r/eclipse-ide-enterprise-java-and-web-developers)
+
+-Scaricare il full JDK 21 di liberica
+(https://bell-sw.com/pages/downloads/#jdk-21-lts)
+
+### Configurare il JDK
+
+una volta aperto eclipse, per utilizzare il JDK di liberica, bisogna andare in alto a sinistra su 
+window > preferences > Java e cliccare su Installed JREs, poi una volta nella schermata cliccare su Add… > next > Directory… e cercare la cartella estratta del JDK (dovrebbe chiamarsi qualcosa del tipo "jdk-21.0.9-full"), una volta selezionata cliccare su finish e selezionare  sulla lista dei JREs il jdk appena aggiunto, infine cliccare su "Apply and close"
+
+### Configurare Tomcat
+
+-per installare il server bisogna andare sulla tabella Servers nella sezione in basso di eclipse (se non presente aggiungerla da Window > Show View) e cliccare su "No servers are available. Click this link to create a new server..."
+selezionare il server "Tomcat v10.1 Server" cliccare su next, poi su Browse... e selezionare la cartella in cui è installato Tomcat, infine cliccare su finish.
--- src/main/java/it/edu/iisgubbio/server/Server.java
+++ src/main/java/it/edu/iisgubbio/server/Server.java
@@ -13,91 +13,142 @@ import jakarta.websocket.server.ServerEndpoint;
 @ServerEndpoint("/nino")
 
 public class Server {
-	//TODO risposta messaggi broadcast
-	//TODO controllo se uno non indenta la risposta correttamente
-	//TODO fa le robe come cristo comanda
+	//TODO fare immagini o gif
+	//TODO se fa prima la M di di fare prima la A
+	//TODO dividere l'if dell'accesso fatto da quello del tipo di messaggio
+	//TODO fare controllo se il paese ha più di 2 caratteri
 	static ArrayList<String> aUtenti = new ArrayList<String>();
 	static ArrayList<Session> aSessione = new ArrayList<Session>();
-	static Map<Session, Boolean> mappaSessioni = new HashMap<>();
+	static Map<Session, Boolean> mappaSessioni = new HashMap<>();//a ogni sessione corrisponde una boolean che dice se quella sessione ha effettuato l'accesso o no
 	@OnMessage
 	public void visualizzaRichiesta(Session session, String msg) {
-		System.out.println("questo, brutto cattivo,Daniele Fiorucci, nato il 20/05/2007 è il messaggio, NON IL BROADCAAST:" + msg);
-//		boolean accesso = false;
-		if(mappaSessioni.get(session)==null) {
+		System.out.println("messaggio: " + msg);
+		if(mappaSessioni.get(session)==null) {//se alla sessione corrisponde null significa che è nuova e va assegnato il valore false
 			mappaSessioni.put(session, false);
 		}
-		//try {
-			if (session.isOpen()) {
-				
-				String vAccesso[] = msg.split("\\|");
-				if(vAccesso[0].equals("A") && mappaSessioni.get(session)==false){
-				String username = vAccesso[1];
-				String pwd = vAccesso[2];
-				System.out.println("nome= "+username);
-				System.out.println("pwd= "+pwd);
+		if (session.isOpen()) {
+			if(!msg.equals("")) {
+				String vMessaggio[] = msg.split("\\|");
+				if(vMessaggio.length==3 || vMessaggio.length==6) {//se il messaggio è una richiesta di accesso deve essere diviso in 3 parametri, se è diviso in 6 è un messaggio altrimenti è scritto male
+			if(vMessaggio[0].equals("A") && mappaSessioni.get(session)==false){//se il messaggio è di tipo accesso e la sessione non ha già effettuato accesso
+				String username = vMessaggio[1];
+				String pwd = vMessaggio[2];
+				System.out.println("nome: " + username);
+				System.out.println("password: " + pwd);
 				if(username.toUpperCase().equals(username)) {
 					if(pwd.charAt(1)=='l') {
 						System.out.println("ENTRATO");
-//						accesso=true;
 						aUtenti.add(username);
 						aSessione.add(session);
 						mappaSessioni.put(session, true);
 						try {
-							System.out.println("utenti nella sessione" + aSessione.size());
+							session.getBasicRemote().sendText("R|ok");
 							for(int i = 0; i<aSessione.size();i++) {
-								aSessione.get(i).getBasicRemote().sendText(broadcast());
+								aSessione.get(i).getBasicRemote().sendText(listaUtenti());
 							}
-//						session.getBasicRemote().sendText(broadcast());
 						} catch(IOException e) {System.out.println(e);}
-//						System.out.println(broadcast());
 					}else {
-						System.out.println("NON ENTRATO");
+						try {
+							session.getBasicRemote().sendText("R|no");
+						} catch (IOException e) {System.out.println(e);}
+						System.out.println("PASSWORD ERRATA");
 					}
 				}else {
-					System.out.println("nome errato");
-					
+					try {
+						session.getBasicRemote().sendText("R|no");
+					} catch (IOException e) {System.out.println(e);}
+					System.out.println("NOME ERRATO");
 				}
-				}else { 
-					if(mappaSessioni.get(session) && vAccesso[0].equals("M")) {
-					String vMessaggio[] = msg.split("\\|");
+			}else { 
+				if(vMessaggio[0].equals("M") && mappaSessioni.get(session)) {//se il messaggio è di tipo messaggio e la sessione ha effettuato l'accesso
 					String nome = vMessaggio[1];
-					String timeStamp = vMessaggio[2];
+//					String timeStamp = vMessaggio[2];
 					String paese = vMessaggio[3];
 					String mediaType = vMessaggio[4];
 					String testo = vMessaggio[5];
+					//"M|nome|ts|paese|mt|testo"
 					
-//					"M|nome|ts|paese|mt|testo"
+						for (int i = 0;i<aSessione.size();i++) {
+							if(session == aSessione.get(i)) {
+								if(!aUtenti.get(i).equals(nome)) {
+									try {
+										session.close();
+									} catch (IOException e) {System.out.println(e);}
+									System.out.println("NOME NON CORRISPONDE A QUELLO FORNITO ALL'ACCESSO");
+									break;
+								}	
+								}
+							}
+						//if timestamp
+						
+					if (!paese.equals("IT")) {
+						try {
+							session.close();
+						} catch (IOException e) {System.out.println(e);}
+						System.out.println("IL TUO PAESE NON È BENVENUTO NELLA NOSTRA CHAT");
+					}
+					if(!mediaType.equals("text/plain")) {
+						try {
+							session.close();
+						} catch (IOException e) {System.out.println(e);}
+						System.out.println("MEDIATYPE ERRATO O NON SUPPORTATO");
+					}
+//					if(testo.equals("")) {
+//						try {
+//							session.close();
+//						} catch (IOException e) {System.out.println(e);}
+//						System.out.println("TESTO VUOTO");
+//					} commentata perchè se il testo è "" lo split non lo conta come elemento da aggiungere a vMessaggio
 					
+						
+						
+						
+						
+						
+					
+					//il nome deve essere uguale al nome dell'accesso
+					//il timestamp deve essere del formato giusto e non avere irregolarità nei dati
+					//il paese deve esse italia senno non funziona / deve essere un paese esistente(difficile)
+					//mediatype deve essere text/plain, oppure anche un png o una gif
+					//il testo non deve eessere vuoto e non essere più lungo di un tot magari
+					try {
+						for(int i = 0; i<aSessione.size();i++) {
+							aSessione.get(i).getBasicRemote().sendText(msg);
+						}
+					} catch(IOException e) {System.out.println(e);}
+				}  else { 
+					try {
+						session.getBasicRemote().sendText("R|no");
+						System.out.println("TIPO DI MESSAGGIO NON ESISTENTE");
+					} catch(IOException e) {System.out.println(e);}
+				
 				}
+						
 			}
+		} else { 
+			try {
+				session.getBasicRemote().sendText("R|no");
+				System.out.println("NUMERO DI PARAMETRI ERRATO");
+			} catch(IOException e) {System.out.println(e);}
+		}
+			} else { 
+				try {
+					session.getBasicRemote().sendText("R|no");
+					System.out.println("MESSAGGIO VUOTO");
+				} catch(IOException e) {System.out.println(e);}
 			}
-//		} catch (IOException e) {
-//			try {
-//
-//				session.close();
-//			} catch (IOException el) {
-//				// niente
-//			}
-//		}
-	
-	
-//    	response.getWriter().write("ciao");q
+		}
     }
-	public String broadcast() {
+	public String listaUtenti() {
 		String nomiUtenti="U";
 		for(int i=0; i<aUtenti.size();i++) {
-			 nomiUtenti+="|" + aUtenti.get(i);
-			 System.out.println("nomi utenti: " + nomiUtenti);
-			 System.out.println("lista utenti: " + aUtenti);
-		
+			 nomiUtenti+="|" + aUtenti.get(i);			
 		}
 		return nomiUtenti;
 	}
 	@OnClose
 	public void chiudiConnessione(Session session) {
-//		System.out.println("disconnesso " + aUtenti.get(i));
-		int i;
-		for (i = 0;i<aSessione.size();i++) {
+		for (int i = 0;i<aSessione.size();i++) {
 			if(session == aSessione.get(i)) {
 				System.out.println(aUtenti.get(i) + " si è disconnesso");
 				mappaSessioni.remove(aSessione.get(i));
@@ -105,7 +156,6 @@ public class Server {
 				aUtenti.remove(i);
 				break;
 			}
-			
 		}
 
 	}
